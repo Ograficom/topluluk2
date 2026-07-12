aeb9d53672f1247cc0e2aaec28b12d1f88616776
turkeshmedia <adembodur149@gmail.com>
2026-07-13 02:01:11 +0300

    Fix frontend initialization crash

--- public/build/assets/Database-D0ao6_9u.js
+++ public/build/assets/Database-D0ao6_9u.js
@@ -40,7 +40,7 @@ import {
 } from "./InstallerLayout-BZOiPsjO.js";
 import {
 	i as M
-} from "./useThemeConfig-safe-82fea8c.js";
+} from "./useThemeConfig-final-9c013f8.js";
 import "./index-T_RycVZ9.js";
 import "./Link-BxabHujp.js";
 import "./_plugin-vue_export-helper-DlAUqK2U.js";
