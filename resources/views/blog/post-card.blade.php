9b119d90da54ad6808983949e2ce4e9a179cecb4
Enes Bodur <enesbodur@ografi.com>
2026-07-15 23:50:10 +0300

    Override global typography on desktop post cards

--- resources/views/blog/post-card.blade.php
+++ resources/views/blog/post-card.blade.php
@@ -8459,18 +8459,23 @@ SVG;
         }
 
         @media (min-width: 1024px) {
-            html body article.post-card[data-post-card-shell] :is(.post-title, .post-title__link) {
+            html body article.post-card[data-post-card-shell] :is(.post-title, .post-title__link):not(#comments *) {
+                font-family: Roboto, Arial, sans-serif !important;
                 font-size: 26px !important;
+                font-weight: 700 !important;
                 line-height: 1.34 !important;
             }
 
-            html body article.post-card[data-post-card-shell] :is(.post-summary, [data-post-card-summary], .post-card__full-content, .post-card__inline-text) {
+            html body article.post-card[data-post-card-shell] :is(.post-summary, [data-post-card-summary], .post-card__full-content, .post-card__inline-text):not(#comments *) {
+                font-family: Roboto, Arial, sans-serif !important;
                 font-size: 20px !important;
+                font-weight: 400 !important;
                 line-height: 1.5 !important;
             }
 
-            html body article.post-card[data-post-card-shell] .expand-link {
+            html body article.post-card[data-post-card-shell] .expand-link:not(#comments *) {
                 font-size: 18px !important;
+                font-weight: 600 !important;
                 line-height: 26px !important;
             }
         }
