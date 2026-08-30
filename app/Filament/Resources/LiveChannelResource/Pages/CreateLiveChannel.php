05d847def001b9aa0ca869eb1bc2318f2d6f4baa
Turkesh <34924312+turkeshmedia@users.noreply.github.com>
2026-08-30 05:22:51 +0300

    Add live channel create page

---
+++ app/Filament/Resources/LiveChannelResource/Pages/CreateLiveChannel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\LiveChannelResource\Pages;
+
+use App\Filament\Resources\LiveChannelResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateLiveChannel extends CreateRecord
+{
+    protected static string $resource = LiveChannelResource::class;
+}
