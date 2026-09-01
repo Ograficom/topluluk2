bf635a7932581f4d11fbe641070d105b396a4b30
Turkesh <34924312+turkeshmedia@users.noreply.github.com>
2026-09-01 05:06:56 +0300

    Register EditorJS inline popover layout fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\DisableDebugbarOnFrontend;
 use App\Http\Middleware\AddSecurityHeaders;
 use App\Http\Middleware\CommentSkeletonPreloaderMiddleware;
 use App\Http\Middleware\ContactPageFieldStyleMiddleware;
+use App\Http\Middleware\EditorJsInlinePopoverLayoutMiddleware;
 use App\Http\Middleware\EditorJsTableInlineFormatMiddleware;
 use App\Http\Middleware\EnsureImageAltText;
 use App\Http\Middleware\EnsureInstalled;
@@ -45,6 +46,7 @@ return Application::configure(basePath: dirname(__DIR__))
             AddSecurityHeaders::class,
             EnsureImageAltText::class,
             EditorJsTableInlineFormatMiddleware::class,
+            EditorJsInlinePopoverLayoutMiddleware::class,
             PostShowTypographyMiddleware::class,
             PostPresentationMiddleware::class,
             PostShowReactionLayoutMiddleware::class,
