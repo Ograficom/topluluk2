7f73bd3e0c5f86ab84b5d3c13bf9deac92c4a0a0
Turkesh <34924312+turkeshmedia@users.noreply.github.com>
2026-09-01 03:35:09 +0300

    Apply contact field styling override

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\EnsureDeviceIdCookie;
 use App\Http\Middleware\DisableDebugbarOnFrontend;
 use App\Http\Middleware\AddSecurityHeaders;
+use App\Http\Middleware\ContactPageFieldStyleMiddleware;
 use App\Http\Middleware\EnsureImageAltText;
 use App\Http\Middleware\EnsureInstalled;
 use App\Http\Middleware\LoginPageSecurityMiddleware;
@@ -44,6 +45,7 @@ return Application::configure(basePath: dirname(__DIR__))
             PostShowReactionLayoutMiddleware::class,
             PostShowCommentIdentityLayoutMiddleware::class,
             LoginPageSecurityMiddleware::class,
+            ContactPageFieldStyleMiddleware::class,
             VideoPageResponseMiddleware::class,
         ]);
     })
