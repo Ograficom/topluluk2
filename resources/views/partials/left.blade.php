172e3775507aead449d208355d952a988a559c3f
Turkesh <34924312+turkeshmedia@users.noreply.github.com>
2026-09-20 01:38:52 +0300

    Remove video navigation

--- resources/views/partials/left.blade.php
+++ resources/views/partials/left.blade.php
@@ -1,7 +1,6 @@
 @php
     $mobileSidebar = $mobileSidebar ?? false;
     $isFeed = request()->routeIs('home');
-    $isVideo = request()->routeIs('video');
     $isFeatured = request()->routeIs('blog.popular');
     $isExplore = request()->routeIs('discover', 'discover.*');
     $isCategories = request()->routeIs('blog.categories', 'blog.category', 'blog.category.*');
@@ -1580,23 +1579,6 @@
                     </a>
                 </li>
                 @endunless
-
-                <li>
-                    <a class="nav-item" href="{{ route('video') }}" data-active="{{ $isVideo ? 'true' : 'false' }}">
-                        <div class="nav-item-icon-outline" aria-hidden="true">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                                <path d="M0 0h24v24H0z" fill="none" />
-                                <g fill="none" stroke="currentColor" stroke-width="1.5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.196 7.873h17.608m-4.997 0V2.877M8.193 7.873V2.877m1.947 9.051v4.922c0 .101.032.2.091.286c.06.085.145.154.246.199a.66.66 0 0 0 .633-.057l3.798-2.65a.56.56 0 0 0 .176-.199a.5.5 0 0 0-.02-.492a.6.6 0 0 0-.192-.186l-3.798-2.272a.66.66 0 0 0-.616-.025a.6.6 0 0 0-.232.198a.5.5 0 0 0-.086.276" />
-                                    <rect width="18.5" height="18.5" x="2.75" y="2.75" rx="6" />
-                                </g>
-                            </svg>
-                        </div>
-                        <div class="nav-item-label-row">
-                            <span class="nav-item-label">News</span>
-                        </div>
-                    </a>
-                </li>
 
                 <li>
                     <a class="nav-item" href="{{ route('blog.popular') }}" data-active="{{ $isFeatured ? 'true' : 'false' }}">
