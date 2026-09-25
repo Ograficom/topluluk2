5c8ddd2744a4615b8a963a79812699b472a1bdee
Turkesh <34924312+turkeshmedia@users.noreply.github.com>
2026-09-25 21:27:08 +0300

    Load homepage feed error state styles

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,7 @@ import '../css/post-create-settings-tabs.css';
 import '../css/post-create-mobile-fix.css';
 import '../css/post-card-footer-polish.css';
 import '../css/post-card-mobile-views-fix.css';
+import '../css/home-feed-error-state.css';
 import '../css/editorjs-create-polish.css';
 import './post-create-server-drafts.js';
 import './stats-modal.js';
